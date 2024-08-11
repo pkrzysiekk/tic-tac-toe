7164ba4d6c67dccc0713bf85ff63eee39cc854f2
pkrzysiekk <pkrzysiek24@gmail.com>
2024-08-11 19:41:02 +0200

    Add main game function that selects a player to start

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const gameBoard=(function(){
 let board=[];
 initiateBoard=()=>{
     for(i=0;i<9;i++){
-        board[i]='O';
+        board[i]='';
     }
 }
 displayBoard=()=>{
@@ -19,8 +19,28 @@ function addPlayer(name,mark){
     const playerMark=mark;
     return {playerName,playerMark}
 }
-gameBoard.initiateBoard();
-gameBoard.displayBoard();
+function getRandomInt(max=2) {
+    return Math.floor(Math.random() * max);
+  }
+function game(player1,player2,gameBoard){
+    console.log("Starting the game!");
+    getStartingPlayer=()=>{
+        let result=getRandomInt();
+        if(result===0){
+            console.log(`${player1.playerName} starts!`)
+            player1.turn=true;
+        }
+        else{
+            console.log(`${player2.playerName} starts!`)
+            player2.turn=true;
+        }
+      
+    }
+    getStartingPlayer();
 
+}
+pkrzysiek=addPlayer('pkrzysiek','X');
+Computer=addPlayer('Computer','Y');
+game(pkrzysiek,Computer,gameBoard.board);
 
 
